586234205572813373de1928a397ab17a8faebe8
LIUYONG <ljy123liu@sina.com>
2016-09-02 10:00:06 +0800

    update

---
+++ Swift04_TableView/Swift04_TableView/CustomCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCell.swift
+//  Swift04_TableView
+//
+//  Created by LIUYONG on 16/7/24.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
