c689fbbe4cbe194dd70367f39526ed00c34573c8
LIUYONG <ljy123liu@sina.com>
2016-09-08 16:55:10 +0800

    Model,TableViewSection

---
+++ Swift12_TableViewSection+Model/Swift12_TableViewSection+Model/CustomHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomHeaderCell.swift
+//  Swift12_TableViewSection+Model
+//
+//  Created by LIUYONG on 16/9/8.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+import UIKit
+
+class CustomHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Swift12_TableViewSection+Model/Swift12_TableViewSection+Model/Person.swift
@@ -0,0 +1,13 @@
+//
+//  Person.swift
+//  Swift12_TableViewSection+Model
+//
+//  Created by LIUYONG on 16/9/8.
+//  Copyright © 2016年 WanJianTechnology. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+}
